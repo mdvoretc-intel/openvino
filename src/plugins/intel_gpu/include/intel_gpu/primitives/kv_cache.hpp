1924613a396caefab10b1546170bc3c20d900042
Dvoretckii, Mikhail <mikhail.dvoretckii@intel.com>
2025-11-19 09:20:40 -0800

    Add a kernel to reorder KV cache

--- src/plugins/intel_gpu/include/intel_gpu/primitives/kv_cache.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/primitives/kv_cache.hpp
@@ -27,17 +27,20 @@ struct kv_cache : public primitive_base<kv_cache> {
              const ov::op::util::VariableInfo& variable_info,
              const int64_t concat_axis,
              const int64_t gather_axis,
-             const bool indirect)
+             const bool indirect,
+             const int64_t trim)
         : primitive_base(id, inputs)
         , variable_info(variable_info)
         , concat_axis(concat_axis)
         , gather_axis(gather_axis)
-        , indirect(indirect) {}
+        , indirect(indirect),
+          trim(trim) {}
 
     ov::op::util::VariableInfo variable_info;
     int64_t concat_axis = 0;
     int64_t gather_axis = 0;
     bool indirect = false;
+    int64_t trim = 0;
 
     bool compressed = false;
     QuantizationAttributes quantization_attributes;
@@ -69,6 +72,7 @@ struct kv_cache : public primitive_base<kv_cache> {
                concat_axis == rhs_casted.concat_axis &&
                gather_axis == rhs_casted.gather_axis &&
                indirect == rhs_casted.indirect &&
+               trim == rhs_casted.trim &&
                compressed == rhs_casted.compressed &&
                quantization_attributes.scales_zp_output_order == rhs_casted.quantization_attributes.scales_zp_output_order &&
                quantization_attributes.output_storage_type == rhs_casted.quantization_attributes.output_storage_type &&
@@ -88,6 +92,7 @@ struct kv_cache : public primitive_base<kv_cache> {
         ob << concat_axis;
         ob << gather_axis;
         ob << indirect;
+        ob << trim;
         ob << compressed;
         ob << make_data(&quantization_attributes.quantization_type, sizeof(quantization_attributes.quantization_type));
         ob << make_data(&quantization_attributes.quantization_dt, sizeof(quantization_attributes.quantization_dt));
@@ -110,6 +115,7 @@ struct kv_cache : public primitive_base<kv_cache> {
         ib >> concat_axis;
         ib >> gather_axis;
         ib >> indirect;
+        ib >> trim;
         ib >> compressed;
         ib >> make_data(&quantization_attributes.quantization_type, sizeof(quantization_attributes.quantization_type));
         ib >> make_data(&quantization_attributes.quantization_dt, sizeof(quantization_attributes.quantization_dt));
--- src/plugins/intel_gpu/src/plugin/ops/kv_cache.cpp
+++ src/plugins/intel_gpu/src/plugin/ops/kv_cache.cpp
@@ -31,7 +31,8 @@ void CreateKVCacheOp(ProgramBuilder& p, const std::shared_ptr<ov::op::internal::
                                 op->get_variable()->get_info(),
                                 ov::util::normalize(op->get_concat_axis(), rank),
                                 ov::util::normalize(op->get_gather_axis(), rank),
-                                op->get_indirect());
+                                op->get_indirect(),
+                                0);
 
     prim.num_outputs = op->get_output_size();
     prim.output_data_types = get_output_data_types(op);
@@ -48,7 +49,8 @@ void CreateKVCacheCompressedOp(ProgramBuilder& p, const std::shared_ptr<ov::op::
                                 op->get_variable()->get_info(),
                                 ov::util::normalize(op->get_concat_axis(), rank),
                                 ov::util::normalize(op->get_gather_axis(), rank),
-                                op->get_indirect());
+                                op->get_indirect(),
+                                0);
 
     prim.compressed = true;
     prim.quantization_attributes = op->get_quantization_attrs();
